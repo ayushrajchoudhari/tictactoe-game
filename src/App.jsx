93a5e8c497b9393ba1964352fa13c00dc97ed65b
Ayush Raj <ayushrajchoudhari@gmail.com>
2024-06-01 17:39:38 +0530

    lifted up the state to App component. Show status message to the user, calculate winner

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,42 @@
 import './styles.scss';
+import { useState } from 'react';
 import Board from './components/Board';
+import { calculateWinner } from './winner';
 
 function App() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(false);
+
+  console.log(squares);
+
+  const winnerPlayer = calculateWinner(squares);
+  const nextPlayer = isXNext ? 'X' : 'O';
+  const statusMessage = winnerPlayer
+    ? `Winner is ${winnerPlayer}`
+    : `Next player is ${nextPlayer}`;
+
+  const handleSquareClick = clickedPosition => {
+    if (squares[clickedPosition] || winnerPlayer) {
+      alert('Restart new game!');
+      return;
+    }
+
+    setSquares(currentSquare => {
+      return currentSquare.map((squareValue, position) => {
+        if (clickedPosition === position) {
+          return isXNext ? 'X' : 'O';
+        }
+        return squareValue;
+      });
+    });
+
+    setIsXNext(currentIsXNext => !currentIsXNext);
+  };
+
   return (
     <div className="app">
-      <Board />
+      <h2> {statusMessage} </h2>
+      <Board squares={squares} handleSquareClick={handleSquareClick} />
     </div>
   );
 }
--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,30 +1,6 @@
-import { useState } from 'react';
-
 import Square from './Square';
 
-const Board = () => {
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [isXNext, setIsXNext] = useState(false);
-
-  console.log(squares);
-
-  const handleSquareClick = clickedPosition => {
-    if (squares[clickedPosition]) {
-      return;
-    }
-
-    setSquares(currentSquare => {
-      return currentSquare.map((squareValue, position) => {
-        if (clickedPosition === position) {
-          return isXNext ? 'X' : 'O';
-        }
-        return squareValue;
-      });
-    });
-
-    setIsXNext(currentIsXNext => !currentIsXNext);
-  };
-
+const Board = ({ squares, handleSquareClick }) => {
   const renderSquare = position => {
     return (
       <Square
